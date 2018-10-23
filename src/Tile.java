2a0fa8dc69871342c181c71c8cb8f9f6c5e55ec3
bjwellsi <31425792+bjwellsi@users.noreply.github.com>
2018-10-23 12:55:24 -0600

    Create Tile.java

---
+++ src/Tile.java
@@ -0,0 +1,52 @@
+
+public class Tile {
+
+	private GamePiece character;
+	private TileType type;
+	boolean hasCharacter;
+
+	public Tile(TileType type, GamePiece character) {
+		this.character = character;
+		this.type = type;
+		hasCharacter = true;
+	}
+
+	public Tile(TileType type) {
+		character = null;
+		this.type = type;
+		hasCharacter = false;
+	}
+
+	public boolean hasCharacter() {
+		return hasCharacter;
+	}
+
+	public TileType getType() {
+		return type;
+	}
+
+	public void setType(TileType type) {
+		this.type = type;
+	}
+
+	public GamePiece getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(GamePiece character) {
+		this.character = character;
+		hasCharacter = true;
+	}
+
+	// has to get called during a move, don't forget this
+	public void removeCharacter() {
+		character = null;
+		hasCharacter = false;
+	}
+
+	public String toString() {
+		if (hasCharacter())
+			return getCharacter().toString();
+		return type.toString();
+	}
+}
