79e2aeb9f4dad9fb8e7bd2dede2aff81cca44d5f
bjwellsi <31425792+bjwellsi@users.noreply.github.com>
2018-10-30 13:02:25 -0600

    Update Ask.java

--- src/main/java/edu/colostate/cs/cs414/cteam/p3/model/Ask.java
+++ src/main/java/edu/colostate/cs/cs414/cteam/p3/model/Ask.java
@@ -2,7 +2,9 @@ package edu.colostate.cs.cs414.cteam.p3.model;
 import java.util.Scanner;
 
 public class Ask {
-
+	
+	
+	//todo close scan
 	public static Move ask(int player, Tile[][] board, String message) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Player " + player + ": \n" + message);
@@ -10,6 +12,41 @@ public class Ask {
 		System.out.println("To attack, type \"x(from),y(from) attack x(to),y(to)\"");
 		// format of an input should be "x,y attack x,y" or "x,y to x,y"
 		String[] line = scan.nextLine().split(",|\\s");
+		if (line.length != 5) {
+			System.out.println("Please insert a valid command!");
+			return null;
+		}
+		int fromX, fromY, toX, toY = 0;
+		try {
+			fromX = Integer.parseInt(line[0]);
+			fromY = Integer.parseInt(line[1]);
+			toX = Integer.parseInt(line[3]);
+			toY = Integer.parseInt(line[4]);
+		} catch (NumberFormatException e) {
+			System.out.println("Please insert a valid command!");
+			return null;
+		}
+
+		if (line[2].equalsIgnoreCase("attack")) {
+			return new Move(fromX, fromY, toX, toY, true);
+		} else if (line[2].equalsIgnoreCase("to")) {
+			return new Move(fromX, fromY, toX, toY, false);
+		} else {
+			System.out.println("Please insert a valid command!");
+			return null;
+		}
+	}
+	
+	public static Move askDemo(int player, Tile[][] board, String message, Scanner scan) throws InterruptedException {
+		Scanner play = new Scanner(System.in);
+		System.out.println("Player " + player + ": \n" + message);
+		System.out.println("To move, type \"x(from),y(from) to x(to),y(to)\"");
+		System.out.println("To attack, type \"x(from),y(from) attack x(to),y(to)\"");
+		// format of an input should be "x,y attack x,y" or "x,y to x,y"
+		String temp = scan.nextLine();
+		System.out.println("\n" + temp + "\n");
+		String ignore = play.nextLine();
+		String[] line = temp.split(",|\\s");
 		if (line.length != 5) {
 			System.out.println("Please insert a valid command!");
 			return null;
