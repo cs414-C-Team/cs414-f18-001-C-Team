e5fa634c306607260840b763a305d62f1a5bc2e8
salonich <43390585+salonich@users.noreply.github.com>
2018-10-30 20:15:22 -0600

    Update Liger_Testcase

--- src/test/java/edu/colostate/cs/cs414/cteam/p3/model/LigerTest.java
+++ src/test/java/edu/colostate/cs/cs414/cteam/p3/model/LigerTest.java
@@ -2,41 +2,87 @@ package edu.colostate.cs.cs414.cteam.p3.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 
 class LigerTest {
-
-
-	GamePiece piece1 = new GamePiece(0,0,null,PieceType.LION ,1);
-	GamePiece piece2 = new GamePiece(1,5,null,PieceType.TIGER ,2);
 	
-	Tile T= new Tile(TileType.BLANK, piece1);
-	
+	Tile[][] board;
 
-	
-
-/*	@Test
-	final void testMove() {
-		fail("Not yet implemented"); // TODO
-	} */
-
-	@Test
-	final void testLiger() {
-		assertTrue(piece1.getTeam()==1);
-		assertTrue(piece1.getX()==0);
-		assertTrue(piece1.getY()==0);
-		assertTrue(piece1.getType()==PieceType.LION);
-		
-		assertTrue(piece2.getTeam()==2);
-		assertTrue(piece2.getX()==1);
-		assertTrue(piece2.getY()==5);
-		assertTrue(piece2.getType()==PieceType.TIGER);
-		
-		//assertThrows(piece1.getType()==PieceType.TIGER);
-		//assertThrows(piece2.getType()==PieceType.LION);
+	@BeforeEach
+	public void initialiseTestBoard() {
+	board = new Tile[9][7];
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 7; j++) {
+			board[i][j] = new Tile(TileType.BLANK);
+		}
 	}
+	// add water tiles
+	for (int i = 3; i < 6; i++) {
+		for (int j = 1; j < 3; j++) {
+			board[i][j].setType(TileType.WATER);
+			board[i][j + 3].setType(TileType.WATER);
+		}
+	}
+
+	// there should really be a better way to do this, but
+	// I have no idea what that would be
+
+	// add dens
+	board[0][3].setType(TileType.DEN);
+	board[8][3].setType(TileType.DEN);
+	// add traps
+	// 0,2 0,4 1,3
+	// 8,2 8,4 7,3
+	board[0][2].setType(TileType.TRAP1);
+	board[0][4].setType(TileType.TRAP1);
+	board[1][3].setType(TileType.TRAP1);
+	board[8][2].setType(TileType.TRAP2);
+	board[8][4].setType(TileType.TRAP2);
+	board[7][3].setType(TileType.TRAP2);
+	// add animals
+     //myTeam
+	board[0][0].setCharacter(new Liger(0, 0, board, PieceType.LION, 1));
+	board[0][6].setCharacter(new Liger(0, 6, board, PieceType.TIGER, 1));
+	
+	//oppositeTeam
+	board[1][6].setCharacter(new Rat(1, 6, board, 2));
+	board[8][0].setCharacter(new Liger(8, 6, board, PieceType.TIGER, 2));
+	
+	
+	
+	
+	}
+	
+	
+	@Test
+	final void moveTest() {
+        
+		GamePiece gp1=board[0][0].getCharacter();   //to check if Lion can move
+		gp1.move(1,0);
+		assertTrue(board[0][0].hasCharacter==false);
+		assertTrue(board[1][0].getCharacter()==gp1);
+		
+		GamePiece gp2 =board[0][6].getCharacter();
+		gp2.move(6,1);
+		assertTrue(board[0][6].hasCharacter==false);
+		assertTrue(board[6][1].getCharacter()==gp2);
+		
+		
+	} 
+	
+/*	@Test 
+	final void attackTest() {
+		GamePiece gp= board[0][6].getCharacter();
+		gp.attack(1, 6);
+		assertTrue(gp.attack(1, 6)==true);
+		
+		
+	}*/
+
+
 	
 	@Test 
 	final void LigerExceptionTest() {
@@ -44,12 +90,14 @@ class LigerTest {
 		
 		assertThrows(IllegalArgumentException.class , () -> {
 			@SuppressWarnings("unused")
-			Liger piece3= new Liger(2,3,null,PieceType.CAT,1);
+			Liger piece = new Liger(2,3,board,PieceType.CAT,1);
+		
 			
 		});
 		
 		
 	}
+
 	
 	
 	
